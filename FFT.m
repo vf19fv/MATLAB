1e67b8353ce83f6b19e9da9119d3e11eebdaea70
vf19fv <y.vf19fv@gmail.com>
2018-03-30 17:43:43 +0900

    いろいろ追加更新

---
+++ FFT.m
@@ -0,0 +1,10 @@
+data = e3.';
+tmp = detrend(data);
+data = transpose(tmp);
+m = length(data);
+n = pow2(nextpow2(m));
+y = fft(data,n);
+f = (0:n-1)*(1/n);
+power = y.*conj(y)/n;
+figure
+plot(f(1:length(f)/2),power(1:length(f)/2))
